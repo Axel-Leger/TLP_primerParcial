38cf770bdeae0f9e428de948cb9252429fddd76b
Axel-Leger <axelleger2@gmail.com>
2025-09-25 11:43:20 -0300

    creacion del repo

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+// import { runAdapter } from "./adapter";
+// import { runAdapter2 } from "./adapter2";
+// import { runFactory } from "./factory";
+// import { runSingletonReservation } from "./Modelo_examen/Reservations";
+// import { runObserver } from "./observer";
+// import { runObserver2 } from "./observer2";
+// import { runSingleton } from "./singleton";
+// import { runSingleton2 } from "./singleton2";
+
+
+(
+  async () => {
+    // await runObserver();
+    // await runObserver2()
+    // await runAdapter();
+    // await runAdapter2()
+    // await runSingleton();
+    // await runSingleton2();
+    // await runFactory();
+
+    // await runSingletonReservation()
+  }
+)();
